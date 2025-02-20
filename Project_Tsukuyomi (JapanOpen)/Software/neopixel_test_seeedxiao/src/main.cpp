ef5c61c315638ffc2eca6aa6f6799e2a882f1d81
asahi_robocuper <asahi.online.2021@gmail.com>
2025-02-20 17:30:45 +0900

    NeoPixelテスト

---
+++ Project_Tsukuyomi (JapanOpen)/Software/neopixel_test_seeedxiao/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(10, 5, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  pixels.setBrightness(100);
+  pixels.begin();
+
+  Serial.begin(9600);
+}
+
+int step_num = 0;
+
+void loop() {
+  for(int i = 0; i < 10; i++)
+  {
+    pixels.clear();
+    pixels.setPixelColor(i, pixels.Color(255, 255, 255));
+    pixels.show();
+  }
+}
