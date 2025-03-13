df34a3d35638f9a68de59f8b03cd752487d72849
ryota kumadaki <ryota.chisa@gmail.com>
2025-03-13 18:50:49 +0900

    回り込み

--- Project_Tsukuyomi (JapanOpen)/Software/main_teensy40/src/process/attacker.cpp
+++ Project_Tsukuyomi (JapanOpen)/Software/main_teensy40/src/process/attacker.cpp
@@ -23,60 +23,96 @@ void process_attacker(int speed)
     }
     else
     {
-        if(ball_deg <= 6 || ball_deg >= 345)
+        if(ball_deg <= 25 || ball_deg >= 335)
         {
-            motor_move(0, 90);
-            f_kicker.kick(600);
-        }
-        else 
-        {
-            if(ball_dis != 0)
-            {             
-                if(ball_deg <= 60)
+            int ball_front[2] = {4, 350};
+            if(ball_dis >= 220)
+            {
+                if(ball_deg <= 4 || ball_deg >= 350)
                 {
-                    motor_move(ball_deg + 45, 60);
-                }
-                else if(ball_deg <= 140)
-                {
-                    motor_move(ball_deg + 30, 70 );
-                }
-                else if(ball_deg <= 180)
-                {
-                    if(ball_dis > 60)
-                    {
-                        motor_move(ball_deg + 80, 70 );
-                    }
-                    else
-                    {
-                        motor_move(ball_deg, 70 );
-                    }
-                }
-                else if(ball_deg <= 220)
-                {
-                    if(ball_dis > 60)
-                    {
-                        motor_move(ball_deg - 80, 70 );
-                    }
-                    else
-                    {
-                        motor_move(ball_deg, 70 );
-                    }
-                    
-                }
-                else if(ball_deg <= 300)
-                {
-                    motor_move(ball_deg - 30, 70 );
+                    motor_move(0, 80);
+                    f_kicker.kick(500);
                 }
                 else
                 {
-                    motor_move(ball_deg - 45, 60);
+                    if(ball_deg <= 25)
+                    {
+                        int ball_deg_diff = ball_deg * 1.7;
+                        float move_scale = (ball_deg_diff - ball_front[0]) / 20.0;
+                        float move_deg = pow(move_scale, 1.5) * ball_deg_diff;
+                        motor_move(move_deg, 65);
+                    }
+                    else if(ball_deg >= 335)
+                    {
+                        int ball_deg_diff = (360 - ball_deg) * 1.7;
+                        float move_scale = (ball_deg_diff - (360 - ball_front[1])) / 20.0;
+                        float move_deg = pow(move_scale, 1.5) * ball_deg_diff;
+                        motor_move(360 - move_deg, 65);
+                    }
+                }
+            }
+            else if(ball_dis >= 30)
+            {
+                if(ball_deg <= 25)
+                {
+                    int ball_deg_diff = ball_deg * 1.7;
+                    float move_scale = (ball_deg_diff - ball_front[0]) / 20.0;
+                    float move_deg = pow(move_scale, 2.0) * ball_deg_diff;
+                    motor_move(move_deg, 65);
+                }
+                else if(ball_deg >= 335)
+                {
+                    int ball_deg_diff = (360 - ball_deg) * 1.7;
+                    float move_scale = (ball_deg_diff - (360 - ball_front[1])) / 25.0;
+                    float move_deg = pow(move_scale, 2.0) * ball_deg_diff;
+                    motor_move(360 - move_deg, 65);
+                }
+                else
+                {
+                    motor_move(ball_deg, 65);
                 }
             }
             else
             {
-                
-                motor_move(ball_deg ,80);
-                
+                motor_move(ball_deg, 80);
+            }
+        }
+        else 
+        {
+            if(ball_dis <= 70 && ball_deg != 0)
+            {
+                motor_move(ball_deg, 80);
+
+            }
+            else
+            {
+                if(ball_deg <= 60)
+                {
+                    motor_move(ball_deg + 40, 65);
+
+                }
+                else if(ball_deg <= 160)
+                {
+
+                    motor_move(ball_deg + 55, 70);
+                }
+                else if(ball_deg <= 180)
+                {
+                    motor_move(ball_deg + 70, 75);
+                }
+                else if(ball_deg <= 200)
+                {
+                    motor_move(ball_deg - 70, 75);
+                }
+                else if(ball_deg <= 300)
+                {
+                    motor_move(ball_deg - 55, 70);
+                }
+                else
+                {
+                    motor_move(ball_deg - 40, 65);
+
+                }
             }
         }
     }
